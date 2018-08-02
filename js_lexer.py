a43e3c1fdb1c4210bd13abadf4c2094e3f311c54
Rob <robmessenger@iMac.lan>
2018-08-02 11:07:38 +0200

    begin JS lexer. subset of tokens

---
+++ js_lexer.py
@@ -0,0 +1,143 @@
+'''
+comments & keywords (lesson 7: prob set 2, #3)
+
+define token definition rules for all tokens in the JS subset except IDENTIFIER,
+NUMBER and STRING
+
+also handle // eol comments and /* */ multiline comments '''
+
+import ply.lex as lex
+import re
+
+tokens = (
+        'ANDAND',       # &&
+        'COMMA',        # ,
+        'DIVIDE',       # /
+        'ELSE',         # else
+        'EQUAL',        # =
+        'EQUALEQUAL',   # ==
+        'FALSE',        # false
+        'FUNCTION',     # function
+        'GE',           # >=
+        'GT',           # >
+        'IF',           # if
+        'LBRACE',       # {
+        'LE',           # <=
+        'LPAREN',       # (
+        'LT',           # <
+        'MINUS',        # -
+        'NOT',          # !
+        'OROR',         # ||
+        'PLUS',         # +
+        'RBRACE',       # }
+        'RETURN',       # return
+        'RPAREN',       # )
+        'SEMICOLON',    # ;
+        'TIMES',        # *
+        'TRUE',         # true
+        'VAR',          # var
+)
+
+def t_VAR(token):
+    r'var'
+    return token
+
+def t_FUNCTION(token):
+    r'function'
+    return token
+
+def t_IF(token):
+    r'if'
+    return token
+
+def t_ELSE(token):
+    r'else'
+    return token
+
+def t_RETURN(token):
+    r'return'
+    return token
+
+def t_LPAREN(token):
+    r'('
+    return token
+
+def t_RPAREN(token):
+    r')'
+    return token
+
+def t_LBRACE(token):
+    r'{'
+    return token
+
+def t_RBRACE(token):
+    r'}'
+    return token
+
+def t_COMMA(token):
+    r','
+    return token
+
+def t_SEMICOLON(token):
+    r';'
+    return token
+
+def t_NOT(token):
+    r'!'
+    return token
+
+def t_ANDAND(token):
+    r'&&'
+    return token
+
+def t_OROR(token):
+    r'\|\|'
+    return token
+
+def t_EQUALEQUAL(token):
+    r'=='
+    return token
+
+def t_GE(token):
+    r'>='
+    return token
+
+def t_LE(token):
+    r'<='
+    return token
+
+def t_GT(token):
+    r'>'
+    return token
+
+def t_LT(token):
+    r'<'
+    return token
+
+def t_EQUAL(token):
+    r'='
+    return token
+
+def t_PLUS(token):
+    r'\+'
+    return token
+
+def t_MINUS(token):
+    r'\-'
+    return token
+
+def t_TIMES(token):
+    r'\*'
+    return token
+
+def t_DIVIDE(token):
+    r'/'
+    return token
+
+def t_TRUE(token):
+    r'true'
+    return token
+
+def t_FALSE(token):
+    r'false'
+    return token
