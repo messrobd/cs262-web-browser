434e96322fb18b724973ff6762c3cfdead204b04
Rob <robmessenger@iMac.lan>
2018-09-11 11:14:08 +0200

    precedence table

--- js_parser.py
+++ js_parser.py
@@ -1,5 +1,10 @@
 def p_binopt(p):
     '''exp: exp PLUS exp
-          | exp MINUS exp 
+          | exp MINUS exp
           | exp TIMES exp''' # TODO add remainder
     p[0] = ('binopt', p[1], p[2], p[3])
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'), # lower precedence to the top. left indicates associativity 
+    ('left', 'TIMES', 'DIVIDE') # higher precedence to the bottom
+)
