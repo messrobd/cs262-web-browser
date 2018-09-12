850c8e8de251529b6a0d2650bad1de72d2ff5fa0
Rob <robmessenger@iMac.lan>
2018-09-12 17:42:57 +0200

    files and changes implementing parsing of JS statements (lesson 16)

--- js_parser.py
+++ js_parser.py
@@ -1,12 +1,119 @@
+'''
+parsing JS statements (lesson 16: prob set 4, #4)
+
+complete the JS parser for statements. grammar:
+    js => element js                                            # starting non-terminal. recursive term
+    js =>                                                       # js base case
+    element => FUNCTION IDENTIFIER ( optparams ) compoundstmt   # function declaration element
+    element => stmt ;                                           # statement element
+    optparams =>                                                # no params
+    optparams => params                                         # >= 1 params
+    params => IDENTIFIER , params                               # >1 params. recursive term
+    params => IDENTIFIER                                        # base case, ==1 params
+    compoundstmt => { statements }                              # body of the func
+    statements => stmt ; statements                             # recursive term
+    statement =>                                                # base case
+    stmt => IF exp compoundstmt
+    stmt => IF exp compoundstmt ELSE compoundstmt
+    stmt => IDENTIFIER = exp
+    stmt => RETURN exp
+    stmt => VAR IDENTIFIER = exp
+    stmt => exp
+    [exp => IDENTIFIER]                                         # exercise treats all exp as identifiers
+'''
+
+start = 'js'    # label the starting non-terminal
+
+def p_js(p):
+        'js : element js'
+        p[0] = [p[1]] + p[2]
+
+def p_js_empty(p):
+        'js : '
+        p[0] = [ ]
+
+# desired parse tree: ('function', name, args, body)
+def p_element_func(p):
+    'element : FUNCTION IDENTIFIER LPAREN optparams RPAREN compoundstmt'
+    p[0] = ('function', p[2], p[4], p[6])
+
+# desired parse tree: ('stmt', stmt)
+def p_element_stmt(p):
+    'element : stmt SEMICOLON'
+    p[0] = ('stmt', p[1])
+
+def p_optparams(p):
+    'optparams : params'
+    p[0] = p[1]
+
+def p_optparams_empty(p):
+    'optparams : '
+    p[0] = [ ]
+
+def p_params(p):
+    'params : IDENTIFIER COMMA params'
+    p[0] = [p[1]] + p[3]
+
+def p_params_last(p):
+    'params : IDENTIFIER'
+    p[0] = [p[1]]
+
+def p_compoundstmt(p):
+    'compoundstmt : LBRACE statements RBRACE'
+    p[0] = p[2]
+
+def p_statements(p):
+    'statements : stmt SEMICOLON statements'
+    p[0] = [p[1]] + p[3]
+
+def p_statements_empty(p):
+    'statements : '
+    p[0] = [ ]
+
+# desired pt: ('if-then', conditional, then_branch)
+def p_stmt_ifthen(p):
+    'stmt : IF exp compoundstmt'
+    p[0] = ('if-then', p[2], p[3])
+
+# desired pt: ('if-then-else', conditional, then_branch, else_branch)
+def p_stmt_ifthenelse(p):
+    'stmt : IF exp compoundstmt ELSE compoundstmt'
+    p[0] = ('if-then-else', p[2], p[3], p[5])
+
+# desired pt: ('assign', identifier, new_value)
+def p_stmt_assign(p):
+    'stmt : IDENTIFIER EQUAL exp'
+    p[0] = ('assign', p[1], p[3])
+
+# desired pt: ('return' exp)
+def p_stmt_return(p):
+    'stmt : RETURN exp'
+    p[0] = ('return', p[2])
+
+# desired pt: ('var', identifier, initial_value)
+def p_stmt_declare(p):
+    'stmt : VAR IDENTIFIER EQUAL exp'
+    p[0] = ('var', p[2], p[4])
+
+# desired pt: ('exp', exp)
+def p_stmt_exp(p):
+    'stmt : exp'
+    p[0] = ('exp', p[1])
+
+def p_exp_identifier(p): # placeholder for exercise 4
+        'exp : IDENTIFIER'
+        p[0] = ("identifier",p[1])
+
+''' to be developed in exercise 5
 precedence = (
     ('left', 'PLUS', 'MINUS'), # lower precedence to the top. left indicates associativity
     ('left', 'TIMES', 'DIVIDE') # higher precedence to the bottom
 )
 
 def p_binopt(p):
-    '''exp: exp PLUS exp
+    \'''exp: exp PLUS exp
           | exp MINUS exp
-          | exp TIMES exp''' # TODO add remainder
+          | exp TIMES exp\''' # TODO add remainder
     p[0] = ('binopt', p[1], p[2], p[3])
 
 def p_exp_call(p):
@@ -32,3 +139,4 @@ def p_args(p):
 def p_args_last(p):
     'args : exp'
     p[0] = [p[1]]
+'''
--- js_parser_tests.py
+++ js_parser_tests.py
@@ -1,8 +1,13 @@
 import ply.lex as lex
+import ply.yacc as yacc
 import js_lexer
 from js_lexer import tokens
+from  js_parser import *
 
-def test_lexer(input_string): #copied this useful func from exercise
+'''
+test procedures (copied from tutorial): '''
+
+def test_lexer(input_string):
   lexer.input(input_string)
   result = [ ]
   while True:
@@ -11,11 +16,16 @@ def test_lexer(input_string): #copied this useful func from exercise
     result = result + [tok.type, tok.value]
   return result
 
-lexer = lex.lex(module=js_lexer)
+def test_parser(input_string):
+    lexer.input(input_string)
+    parse_tree = parser.parse(input_string,lexer=lexer)
+    return parse_tree
 
+lexer = lex.lex(module=js_lexer)
+parser = yacc.yacc()
+'''
 test_tokens = """ - !  && () * , / ; { || } + < <= = == > >= else false function
 if return true var """
-
 print test_lexer(test_tokens)
 
 comments = """
@@ -23,12 +33,47 @@ if // else mystery
 =/*=*/=
 true /* false
 */ return"""
-
 print test_lexer(comments)
+
+false_prophets = 'false_prophets "false_prophets true prophets"'
+print test_lexer(false_prophets)
 
 input1 = 'some_identifier -12.34 "a \\"escape\\" b"'
 print test_lexer(input1)
 
-
 input2 = '-12x34'
 print test_lexer(input2)
+'''
+js_simple_stmt = 'var president = trump;'
+#print test_lexer(js_simple_stmt)
+print test_parser(js_simple_stmt)
+
+js_simple_func = "function myfun() { return nothing ; }"
+#print test_lexer(js_simple_func)
+print test_parser(js_simple_func)
+
+js_func_args = "function nobletruths(dukkha,samudaya,nirodha,gamini) { return buddhism ; }"
+print test_parser(js_func_args)
+
+js_multi_top ="""var view = right;
+var intention = right;
+var speech = right;
+action = right;
+livelihood = right;
+effort_right;
+mindfulness_right;
+concentration_right;"""
+print test_parser(js_multi_top)
+
+js_if_else = """
+if cherry {
+  orchard;
+  if uncle_vanya {
+    anton ;
+    chekov ;
+  } else {
+  } ;
+  nineteen_oh_four ;
+} ;
+"""
+print test_parser(js_if_else)
