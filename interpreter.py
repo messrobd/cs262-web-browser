21846c3c8e18313a63d9e29a18f37db2941fc93a
Rob <messrobd@Robs-MacBook.local>
2018-09-20 09:47:25 +0200

    js interpreter: eval_exp binops with variables

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,9 @@
 '''
 procedure is to provide words ("word-element","Hello") to a rendering component
-(missing) and to extract and validate tags ('tag-element', 'b', [], [], 'b')]'''
+(missing) and to extract and validate tags ('tag-element', 'b', [], [], 'b')]
+
+missing: don't yet see how we are going tp transition from html to js
+interpreter '''
 
 def interpret_html(trees):
     for tree in trees:
@@ -18,3 +21,36 @@ def interpret_html(trees):
                     return tagname + ' ' + interpret_html(subtrees)
                 except Exception as problem:
                     print problem
+
+def eval_exp(tree, environment):
+    nodetype = tree[0]
+    if nodetype == "number":
+        return float(tree[1])
+    elif nodetype == "string":
+        return tree[1]
+    elif nodetype == "true":
+        return True
+    elif nodetype == "false":
+        return False
+    elif nodetype == "not":
+        return not(eval_exp(tree[1], environment))
+    elif nodetype == "binop":
+        (left_child, operator, right_child) = tree[1:]
+        x = eval_exp(left_child, environment)
+        y = eval_exp(right_child, environment)
+        return perform_binop(x, operator, y)
+    elif nodetype == "identifier":
+        return env_lookup(environment, tree[1])
+
+def perform_binop(x, operator, y):
+    if operator == '+':
+        return x + y
+    elif operator == '-':
+        return x - y
+    elif operator == '*':
+        return x * y
+    elif operator == '/':
+        return x / y
+    
+def env_lookup(env,vname):
+        return env.get(vname,None)
--- interpreter_tests.py
+++ interpreter_tests.py
@@ -8,3 +8,16 @@ print interpret_html(html_body)
 
 html_negative = [('tag-element', 'body', [], [('tag-element', 'i', [], [('word-element', 'Hello World')], 'b')], 'body')]
 print interpret_html(html_negative)
+
+test_tree1 = ("binop",("number","5"),"+",("number","8"))
+print eval_exp(test_tree1, {}) # 13
+
+test_tree2 = ("number","1776")
+print eval_exp(test_tree2, {}) # 1776
+
+test_tree3 = ("binop",("number","5"),"+",("binop",("number","7"),"-",("number","18")))
+print eval_exp(test_tree3, {}) # -6
+
+environment = {"x" : 2}
+tree = ("binop", ("identifier","x"), "+", ("number","2"))
+print eval_exp(tree,environment) # 4
