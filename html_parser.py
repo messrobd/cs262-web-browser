e38bba2e9ce9ae76e9c668986fccfc5ab9dc3c01
Rob <robmessenger@iMac.lan>
2018-10-03 10:46:04 +0200

    parse js token (trivial token => ptree)

--- html_parser.py
+++ html_parser.py
@@ -6,16 +6,16 @@ elt => WORD      # an element can be a word, which is a terminal
 elt => TAG       # tag is a terminal '''
 
 def p_html(p): # p_ is the flag prefix for a parse rule
-    'html: elt html' # my guess this is interpreted as an object
+    'html : elt html' # my guess this is interpreted as an object
     p[0] = [p[1]] + p[2] # p[0] refers to 'html' on the lhs (is looking up from the 'object'?)
                      # p[1] refers to elt, and p[2] to the remaining html, return 1 list
 
 def p_html_empty(p):
-    'html:'
+    'html : '
     p[0] = ''
 
 def p_elt_word(p):
-    'elt: WORD' # capitalisation signifies a TOKEN (previously defined)
+    'elt : WORD' # capitalisation signifies a TOKEN (previously defined)
     p[0] = ('word_elt', p[1]) # I think this will always substitute, rather than nesting
 
 '''
@@ -25,8 +25,12 @@ parse_tree = [
     ('word_elt', 'Rama's),
     ('word_elt', 'journey')] '''
 
+def p_elt_javascript(p):
+    'elt : JAVASCRIPT'
+    p[0] = ('javascript_elt', [1])
+
 def p_elt_tag(p):
-    'elt: LANGLE WORD tag_args RANGLE html LANGLESLASH WORD RANGLE'
+    'elt : LANGLE WORD tag_args RANGLE html LANGLESLASH WORD RANGLE'
     p[0] = ('tag_elt', p[2], p[3], p[5], p[7]) # WORD's are terminals, tag_args & html get expanded
 
 '''
